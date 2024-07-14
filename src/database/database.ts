8a5d515f306f42fd7320bc73cfcac0e0b646ed9f
Baenat <bae.julian10@gmail.com>
2024-07-14 03:03:38 -0500

    Config database mongoDB

---
+++ src/database/database.ts
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const dbConnect = (): Promise<any> => {
+  const DB_URI = process.env.BD_URI;
+
+  return new Promise<boolean>((resolve, reject) => {
+    mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then(() => {
+      console.log("Successfully connected to the database");
+      resolve(true);
+    }).catch((err: Error) => {
+      console.log('Could not connect to the database. Exiting now...', err.message);
+      process.exit(1);
+    });
+  })
+
+}
+
+export = dbConnect;
